01c3ad509bdb20b33cc79ea99a368c2f1d8b86a5
CSMore <erickaro98@gmail.com>
2025-07-11 22:00:13 -0600

    configuracion general y cambio de estructura necesaria

--- database/connection.py
+++ database/connection.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Conexión a MySQL con PyMySQL
+#DB_URL = "mysql+pymysql://root:Rootpw_2508%40@127.0.0.1:3306/gestor_tareas_app"
 DB_URL = "mysql+pymysql://root:123Queso@127.0.0.1:3306/gestor_tareas_app"
 
 engine = create_engine(DB_URL, echo=False, pool_pre_ping=True)
